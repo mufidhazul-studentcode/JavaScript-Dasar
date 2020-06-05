3f474a65bace9354200484b410e7be5f97d00184
Mufidha Zulfulansyah <mufidhazulfulansyah@outlook.co.id>
2020-06-05 21:49:08 +0700

    Wesla semene sek, ngantuk

---
+++ 15.MenulisJavaScript/script.js
@@ -0,0 +1,3 @@
+var x = 10;
+console.log('Hello Donyo!');
+console.log('isi dari variabel x adalah ' + x);
---
+++ 16.PopUpBoxJavaScript/script.js
@@ -0,0 +1,13 @@
+alert('Hai brad/sist!');
+alert('Nama saya Mufidha Zul')
+alert('Kenalan kuy~')
+var lagi = true;
+
+while (lagi) {
+	var nama = prompt('Namamu syapa?');
+	alert('Halo ' + nama + ', aku Mufidha. Salken yah :))');
+
+	lagi = confirm('Mau ganti nama?');
+}
+
+alert('Yah, udahan dulu ya... Thankyou~');
